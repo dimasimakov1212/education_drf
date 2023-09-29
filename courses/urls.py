a4a5fc89610bfa2f001e7068263981b6eafd343b
DimaSimakov <dimarodeo@gmail.com>
2023-09-29 17:25:16 +0700

    Created CourseViewSet in views.py

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
 from courses.apps import CoursesConfig
+from rest_framework.routers import DefaultRouter
 
+from courses.views import CourseViewSet
 
 app_name = CoursesConfig.name
 
+router = DefaultRouter()
+router.register(r'course', CourseViewSet, basename='course')
+
 urlpatterns = [
     # path('', LoginView.as_view(template_name='users/login.html'), name='login'),
-    ]
+    ] + router.urls
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from courses.models import Course
+from courses.serializers import CourseSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet-класс для вывода списка авто и информации по одному объекту
+    """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
