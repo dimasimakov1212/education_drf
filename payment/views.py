8ad7eaf1a176d24dce9c5054c1d363525efc7356
DimaSimakov <dimarodeo@gmail.com>
2023-10-03 15:37:53 +0700

    Add filters in PaymentListAPIView

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 
 from payment.models import Payment
 from payment.serializers import PaymentSerializer
@@ -11,6 +13,10 @@ class PaymentListAPIView(generics.ListAPIView):
     """
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+
+    filter_backends = [DjangoFilterBackend, OrderingFilter]  # Бэкенд для обработки фильтра
+    filterset_fields = ('course', 'lesson', 'payment_type')  # Набор полей для фильтрации
+    ordering_fields = ['payment_date']
 
 
 class PaymentCreateAPIView(generics.CreateAPIView):
