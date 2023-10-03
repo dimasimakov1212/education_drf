c050b674159f6fa0c0b542d0d5ea0f9a6f6c5bb9
DimaSimakov <dimarodeo@gmail.com>
2023-10-03 21:41:16 +0700

    Add payments field in UserSerializer

--- users/urls.py
+++ users/urls.py
@@ -12,7 +12,7 @@ router = DefaultRouter()
 
 urlpatterns = [
     path('user/create/', UserCreateAPIView.as_view(), name='user_create'),
-    path('user/', UserListAPIView.as_view(), name='user_list'),
+    path('', UserListAPIView.as_view(), name='user_list'),
     path('user/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_detail'),
     path('user/update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_change'),
     path('user/delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user_delete'),
