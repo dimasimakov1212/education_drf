1cd5b69c1631db0f6a6bb12d47daf51047b1384a
DimaSimakov <dimarodeo@gmail.com>
2023-10-03 15:09:18 +0700

    Created get_lessons method to CourseSerializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,12 +3,23 @@ from rest_framework import serializers
 from courses.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    """
+    Класс сериализатора для модели Lesson
+    """
+    class Meta:
+        model = Lesson
+        # fields = ('lesson_title', 'lesson_description',)
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     """
     Класс сериализатора для модели Course
     """
     # определяем дополнительное поле в модели Course
     lessons_count = serializers.SerializerMethodField()  # данные о количестве уроков в курсе
+    lessons = serializers.SerializerMethodField()  # данные о уроках курса
 
     class Meta:
         model = Course
@@ -22,12 +33,10 @@ class CourseSerializer(serializers.ModelSerializer):
         """
         return instance.lesson.all().count()
 
-
-class LessonSerializer(serializers.ModelSerializer):
-    """
-    Класс сериализатора для модели Course
-    """
-    class Meta:
-        model = Lesson
-        # fields = ('lesson_title', 'lesson_description',)
-        fields = '__all__'
+    def get_lessons(self, course):
+        """
+        Метод определения поля lessons
+        :return: список уроков курса
+        """
+        lessons = [lesson.lesson_title for lesson in Lesson.objects.filter(course=course)]
+        return lessons
